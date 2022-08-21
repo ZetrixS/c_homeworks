20dc8827e2d38d9f967b001df52cc5f24ea80ffd
Ivan <zetriss@gmail.com>
2022-08-21 18:57:59 +0300

    Выполнение третьего задания

---
+++ homework_1/task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = 0;
+Console.Write("Введите число:");
+string input = Console.ReadLine();
+number = Convert.ToInt32(input);
+if (number%2 == 0)
+{
+    Console.Write("Число " + number + " - четное.");
+}
+else
+{
+    Console.Write("Число " + number + " - не четное." );
+}
