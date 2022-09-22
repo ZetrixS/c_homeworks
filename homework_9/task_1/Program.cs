3a01d7f0bf001a7d07b12919d25a376677c5f5b8
Ivan <zetriss@gmail.com>
2022-09-22 09:05:11 +0300

    Выполнение первого задания

---
+++ homework_9/task_1/Program.cs
@@ -0,0 +1,60 @@
+﻿//// Задача 1: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+////Блок 1. Ввод данных и вывод полученного резултата
+Console.Write("Введите количество строк: ");
+int lines = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[lines, columns];
+massive_random_numbers(numbers);
+Console.WriteLine();
+Console.WriteLine("Массив до изменения: ");
+print_massive(numbers);
+
+//// Блок 2. Выполнения условий задачи
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+    {
+        for (int z = 0; z < numbers.GetLength(1) - 1; z++)
+        {
+            if (numbers[i, z] < numbers[i, z + 1]) //для изменения сортировки поменять знак < на противоположный
+            {
+                int temp = 0;
+                temp = numbers[i, z];
+                numbers[i, z] = numbers[i, z + 1];
+                numbers[i, z + 1] = temp;
+            }
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Массив с упорядоченными по убыванию значениями: ");
+print_massive(numbers);
+
+
+
+//// Блок 3. Функции
+void massive_random_numbers(int[,] massive)
+{
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            massive[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void print_massive(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
